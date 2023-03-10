995044854307a1138513155460c7b6faf9f353ab
Hossein Abdollahi <abdollahi_hossein@yahoo.com>
2023-03-10 12:17:57 -0500

    Making a useLogin Hook

--- mern/client/src/pages/Login.js
+++ mern/client/src/pages/Login.js
@@ -1,20 +1,22 @@
 import { useState } from "react"
+import { useLogin } from "../hooks/useLogin"
 
 const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const {login, error, isLoading} = useLogin()
 
   const handleSubmit = async (e) => {
     e.preventDefault()
 
-    console.log(email, password)
+    await login(email, password)
   }
 
   return (
     <form className="login" onSubmit={handleSubmit}>
       <h3>Log In</h3>
       
-      <label>Email:</label>
+      <label>Email address:</label>
       <input 
         type="email" 
         onChange={(e) => setEmail(e.target.value)} 
@@ -26,9 +28,9 @@ const Login = () => {
         onChange={(e) => setPassword(e.target.value)} 
         value={password} 
       />
-      <br/>
-      <br/>
-      <button>Log in</button>
+
+      <button disabled={isLoading}>Log in</button>
+      {error && <div className="error">{error}</div>}
     </form>
   )
 }
