3b84c061c622274a009dc2698e6b3ed230784e68
Hossein Abdollahi <abdollahi_hossein@yahoo.com>
2023-03-09 22:27:12 -0500

    Signing Up & Hashing Passwords

--- mern/server/controllers/userController.js
+++ mern/server/controllers/userController.js
@@ -1,4 +1,5 @@
-const User = require('../models/userModel')
+const dbo = require("../db/conn");
+const bcrypt = require('bcrypt')
 
 // login a user
 const loginUser = async (req, res) => {
@@ -6,8 +7,28 @@ const loginUser = async (req, res) => {
 }
 
 // signup a user
-const signupUser = async (req, res) => {
-  res.json({mssg: 'signup user'})
+const signupUser = async (req, response) => {
+  let db_connect = dbo.getDb("employees");
+
+  let first_name = req.body.first_name
+  let last_name = req.body.last_name
+  let email = req.body.email
+  let password = req.body.password
+    
+  let count = await db_connect.collection("users").countDocuments({email})
+
+  if (count != 0) {
+    response.json('Email already in use')
+    return
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(password, salt)
+
+  db_connect.collection("users").insertOne({ first_name, last_name, email, password: hash }, function (err, res) {
+    if (err) throw err;
+    response.json({ first_name, last_name, email, password: hash });
+  });
 }
 
 module.exports = { signupUser, loginUser }
