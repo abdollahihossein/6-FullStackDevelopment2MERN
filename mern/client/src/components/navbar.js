d06a30f18dc8213d81edb837aa405655ba1c94a6
Hossein Abdollahi <abdollahi_hossein@yahoo.com>
2023-03-14 12:14:09 -0400

    Moving create agent link from navbar to recordlist

--- mern/client/src/components/navbar.js
+++ mern/client/src/components/navbar.js
@@ -34,7 +34,7 @@ export default function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        {/* <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
               <NavLink className="nav-link" to="/create">
@@ -42,7 +42,7 @@ export default function Navbar() {
               </NavLink>
             </li>
           </ul>
-        </div>
+        </div> */}
 
         {user && (
             <div style={{ padding: 30 }}>
--- mern/client/src/components/recordList.js
+++ mern/client/src/components/recordList.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { useAuthContext } from "../hooks/useAuthContext";
 import Alertsuccess from '../alerts/alertsuccess'
+
+import "bootstrap/dist/css/bootstrap.css";
 
 const Record = (props) => (
   <tr>
@@ -92,7 +95,18 @@ export default function RecordList() {
   return (
     <div>
       <Alertsuccess modalshow={show} setShow={setShow} text={text}/>
-      <h3>Agent List</h3>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/create">
+                Create Agent
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <h3>Agent Management</h3>
       <table className="table table-striped" style={{ marginTop: 20 }}>
         <thead>
           <tr>
